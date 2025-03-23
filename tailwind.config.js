0c97140a61b7055986da6dfce549c928fb70d508
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-03-23 11:32:00 +0530

    chore: initial setup with React and  TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
